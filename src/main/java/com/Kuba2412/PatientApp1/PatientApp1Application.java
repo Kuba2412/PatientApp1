68038a7b012b981396d0561b5ebc8f9da5174c8d
Kuba Poswiatowski <xkubusx5@gmail.com>
2024-07-25 16:19:24 +0200

    first commit

---
+++ src/main/java/com/Kuba2412/PatientApp1/PatientApp1Application.java
@@ -0,0 +1,13 @@
+package com.Kuba2412.PatientApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kuba2412/PatientApp1/PatientApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kuba2412.PatientApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
